e1ed24151c849ecdccd76d91867de0f71547106f
ChunyuBai <baichunyu26@gmail.com>
2022-12-15 15:41:38 -0500

    refactory getAllReservations function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -77,7 +77,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.*, properties.*
+  FROM reservations
+  JOIN properties ON properties.id = property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY reservations.id, properties.id
+  LIMIT $2;`,[guest_id,limit])
+  .then(result => {
+    return result.rows;
+  })
 }
 exports.getAllReservations = getAllReservations;
 
