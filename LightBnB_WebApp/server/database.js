37750dec4dd6f981b3c965b3692d5b81ebcb9cce
ChunyuBai <baichunyu26@gmail.com>
2022-12-15 15:02:53 -0500

    refactoring getUserById and getUserByEmail and addUser functions

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -19,16 +19,16 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
+  return pool
+  .query(`SELECT * FROM users WHERE email = $1`,
+  [email.toLocaleLowerCase()])
+  .then(result => {
+    if(result.rows){
+      return result.rows[0];
     } else {
-      user = null;
+      return null;
     }
-  }
-  return Promise.resolve(user);
+  })
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -38,10 +38,18 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1`,
+  [id])
+  .then(result => {
+    if(result.rows){
+      return result.rows[0];
+    } else {
+      return null
+    }
+  })
 }
 exports.getUserWithId = getUserWithId;
-
 
 /**
  * Add a new user to the database.
@@ -49,12 +57,17 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+  .query(`INSERT INTO users(name, email, password)
+  VALUES($1,$2,$3)
+  RETURNING *;`,
+  [user.name, user.email, user.password])
+  .then(result => { 
+    return result.rows[0];
+  })
 }
 exports.addUser = addUser;
+
 
 /// Reservations
 
@@ -80,7 +93,6 @@ exports.getAllReservations = getAllReservations;
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
